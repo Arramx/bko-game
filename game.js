45a17267c2bc4af7ee4d13287e23b409a5f6bdad
Arramx <arram@tutanota.com>
2020-08-16 14:04:50 +0200

    No need to hold mouse

--- game.js
+++ game.js
@@ -2,7 +2,6 @@ let objs = [];
 let hp = 3;
 const maxHP = hp;
 let score = 0;
-let mouseClick = false;
 const hpBar = document.querySelector('.hp-fill');
 const scorePointer = document.querySelector('.score');
 
@@ -13,40 +12,39 @@ const gameInterval = setInterval(() => {
     objs.push(new Rectangle(0.12, 0.15, vertexSrc, fragmentSrc, 'file.png', 'rgba', favourite > 7));
 }, 500);
 
-document.addEventListener('mousedown', () => mouseClick = true);
-document.addEventListener('mouseup', () => mouseClick = false);
+const lowerHP = () => {
+    hp--;
+    hpBar.style.width = `${Math.round(hp/maxHP*100)}%`;
+
+    if (hp === 0) {
+        clearInterval(gameInterval);
+        objs = [];
+        document.querySelector('.title').innerHTML = `Wynik: ${score}`;
+        document.querySelector('.container').style.display = 'block';
+    }
+}
+
 document.addEventListener('mousemove', e => {
-    if (mouseClick) {
-        for (const obj of objs) {
-            if (!obj.clicked) {
-                const x1 = (1+obj.pos[0])*canvas.width/2;
-                const x2 = x1 + obj.widthP*canvas.width/2;
-                const y1 = (1-obj.pos[1])*canvas.height/2;
-                const y2 = y1 + obj.heightP*canvas.height/2;
-    
-                if (e.clientX > x1 && e.clientX < x2 && e.clientY > y1 && e.clientY < y2) {
-                    obj.clicked = true;
-                    if (obj.favourite) {
-                        hp--;
-                        hpBar.style.width = `${Math.round(hp/maxHP*100)}%`;
+    for (const obj of objs) {
+        if (!obj.clicked) {
+            const x1 = (1+obj.pos[0])*canvas.width/2;
+            const x2 = x1 + obj.widthP*canvas.width/2;
+            const y1 = (1-obj.pos[1])*canvas.height/2;
+            const y2 = y1 + obj.heightP*canvas.height/2;
 
-                        if (hp === 0) {
-                            clearInterval(gameInterval);
-                            objs = [];
-                            document.querySelector('.title').innerHTML = `Wynik: ${score}`;
-                            document.querySelector('.container').style.display = 'block';
-                            break;
-                        }
-
-                    } else {
-                        score++;
-                        scorePointer.innerHTML = score;
-                    }
-                    obj.fade();
+            if (e.clientX > x1 && e.clientX < x2 && e.clientY > y1 && e.clientY < y2) {
+                obj.clicked = true;
+                if (obj.favourite) {
+                    lowerHP();
+                } else {
+                    score++;
+                    scorePointer.innerHTML = score;
                 }
+                obj.fade();
             }
         }
     }
+    
 });
 
 const animate = () => {
@@ -61,6 +59,10 @@ const animate = () => {
     for (const obj of objs) {
         obj.move();
         obj.draw();
+
+        if (obj.pos[1] < -1 && obj.wasUp && !obj.favourite) {
+            lowerHP();
+        }
     }
 
     objs = objs.filter(a => !a.out);
